6cb182f50f9485d7a80ea619d3f24746e00963f4
Olosko23 <oloogeorge633@gmail.com>
2023-08-09 08:58:04 +0300

    'redundant_files'

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const router = express.Router();
-//const jwt = require("jsonwebtoken");
-//const jobController = require("../controllers/jobController");
-//const { protect } = require("../middlewares/authMiddleware");
+const jobController = require("../controllers/jobController");
+const { protect } = require("../middlewares/authMiddleware");
 
-// Create a job posting
-//router.post("/", verifyToken, jobController.createJob);
-// Get all job postings
-//router.get("/", jobController.getAllJobs);
-
-//router.route("/").get( jobController.getAllJobs).post( jobController.createJob);
+router
+  .route("/jobs")
+  .get(protect, jobController.getAllJobs)
+  .post(protect, jobController.createJob);
 
 module.exports = router;
