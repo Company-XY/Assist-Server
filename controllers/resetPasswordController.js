33a850f879f35b37387fcd50e20c1760c3147ac2
Olosko23 <oloogeorge633@gmail.com>
2023-08-22 20:44:57 +0300

    'password'

--- controllers/resetPasswordController.js
+++ controllers/resetPasswordController.js
@@ -25,7 +25,7 @@ const sentResetLink = asyncHandler(async (req, res) => {
     // Send reset password email using Elastic Email API
     const resetLink = `https://beta-assist.netlify.app/password/${resetToken}`;
     const data = {
-      from: "assist.africa@info.com",
+      from: "oloogeorge633@gmail.com",
       to: email,
       subject: "Password Reset",
       bodyText: `Click the link to reset your password: ${resetLink}`,
